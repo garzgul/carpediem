e38044c4bef4dbbd274277e09abe32d9a73a5cd7
cdi116 <cdi116@pltz901.AFPA-PARIS-DI>
2014-09-15 11:14:14 +0200

    Merge origin/Gael into Gael
    Conflicts:
    	src/java/bean/acheteur/Acheteur.java

--- src/java/servlet/Controleur.java
+++ src/java/servlet/Controleur.java
@@ -621,7 +621,7 @@ public class Controleur extends HttpServlet {
 
         
     }
-
+    
 // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
